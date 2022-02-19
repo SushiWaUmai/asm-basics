d5a95317d2d8f1879875bc725c4a6a95df95b7cb
SushiWaUmai <eugene.bb1@gmail.com>
2022-02-19 18:24:31 +0100

    feat: :sparkles: move length to variable

--- hello-world/main.asm
+++ hello-world/main.asm
@@ -1,5 +1,6 @@
 section .data
   msg db "Hello, World!", 10 ; 10 = \n LF EOF
+  len equ $ - msg
 
 section .text
   global _start
@@ -8,7 +9,7 @@ _start:
   mov eax, 1        ; system call of 1 is write
   mov edi, 1        ; set output stream to stdout
   mov esi, msg      ; set adress of the string to output
-  mov edx, 14       ; number of the bytes of the message including the null terminiator
+  mov edx, len      ; number of the bytes of the message including the null terminiator
   syscall
 
   mov eax, 60       ; system call for exit
