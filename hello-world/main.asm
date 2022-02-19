496bdcf761e57e2c9e1f052bb5fd1fabfba11b8a
SushiWaUmai <eugene.bb1@gmail.com>
2022-02-19 18:57:35 +0100

    fix: :sparkles: update to 64 bit registers

--- hello-world/main.asm
+++ hello-world/main.asm
@@ -6,13 +6,13 @@ section .text
   global _start
 
 _start:
-  mov eax, 1        ; system call of 1 is write
-  mov edi, 1        ; set output stream to stdout
-  mov esi, msg      ; set adress of the string to output
-  mov edx, len      ; number of the bytes of the message including the null terminiator
+  mov rax, 1        ; system call of 1 is write
+  mov rdi, 1        ; set output stream to stdout
+  mov rsi, msg      ; set adress of the string to output
+  mov rdx, len      ; number of the bytes of the message including the null terminiator
   syscall
 
-  mov eax, 60       ; system call for exit
-  mov edi, 0        ; set return code to 0
+  mov rax, 60       ; system call for exit
+  mov rdi, 0        ; set return code to 0
   syscall
 
