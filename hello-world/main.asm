a72530eae6ddf752c8565fc4ddc8f1aee2ea71e5
SushiWaUmai <eugene.bb1@gmail.com>
2022-05-23 21:32:03 +0200

    style: :art: format code

--- hello-world/main.asm
+++ hello-world/main.asm
@@ -1,18 +1,17 @@
 section .data
-  msg db "Hello, World!", 10 ; 10 = \n LF EOF
-  len equ $ - msg
+msg     db "Hello, World!", 10; 10 = \n LF EOF
+len     equ $ - msg
 
 section .text
-  global _start
+global  _start
 
 _start:
-  mov rax, 1        ; system call of 1 is write
-  mov rdi, 1        ; set output stream to stdout
-  mov rsi, msg      ; set adress of the string to output
-  mov rdx, len      ; number of the bytes of the message including the null terminiator
-  syscall
+	mov rax, 1; system call of 1 is write
+	mov rdi, 1; set output stream to stdout
+	mov rsi, msg; set adress of the string to output
+	mov rdx, len; number of the bytes of the message including the null terminiator
+	syscall
 
-  mov rax, 60       ; system call for exit
-  mov rdi, 0        ; set return code to 0
-  syscall
-
+	mov rax, 60; system call for exit
+	mov rdi, 0; set return code to 0
+	syscall
--- triangle/main.asm
+++ triangle/main.asm
@@ -1,57 +1,56 @@
 section .data
-  star db "*"         ; star
-  new_line db 10      ; \n
+star    db "*"; star
+new_line db 10      ; \n
 
 section .text
-  global _start
+global  _start
 
 _start:
-  xor r11, r11        ; set the r11 counter to 0
+	xor r11, r11; set the r11 counter to 0
 
-  call loop1
-  call end
-
+	call loop1
+	call end
 
 loop1:
-  mov rax, 1          ; system call of 1 is write
-  mov rdi, 1          ; set output stream to stdout
-  mov rsi, star       ; set adress of the string to *
-  mov rdx, 1          ; add length of string
+	mov rax, 1; system call of 1 is write
+	mov rdi, 1; set output stream to stdout
+	mov rsi, star; set adress of the string to *
+	mov rdx, 1; add length of string
 
-  xor rcx, rcx        ; set rcx counter to 0
-  call loop2          ; call the inner loop
+	xor  rcx, rcx; set rcx counter to 0
+	call loop2; call the inner loop
 
-  mov rax, 1          ; system call of 1 is write
-  mov rdi, 1          ; set output stream to stdout
-  mov rsi, new_line   ; set adress of the string to \n
-  mov rdx, 1          ; add string
+	mov rax, 1; system call of 1 is write
+	mov rdi, 1; set output stream to stdout
+	mov rsi, new_line; set adress of the string to \n
+	mov rdx, 1; add string
 
-  push r11            ; push r11 before syscall
-  syscall
-  pop r11             ; pop r11 after syscall
+	push r11; push r11 before syscall
+	syscall
+	pop  r11; pop r11 after syscall
 
-  inc r11             ; increment r11 by 1
-  cmp r11, 100        ; if the r11 counter is less than 5
-  jl loop1            ; jump to loop1
-  ret
+	inc r11; increment r11 by 1
+	cmp r11, 100; if the r11 counter is less than 5
+	jl  loop1; jump to loop1
+	ret
 
 loop2:
-  ; push all persistent registers before syscall
-  push rcx
-  push r11
+	;    push all persistent registers before syscall
+	push rcx
+	push r11
 
-  syscall
+	syscall
 
-  ; pop all persistent registers after syscall
-  pop r11
-  pop rcx
+	;   pop all persistent registers after syscall
+	pop r11
+	pop rcx
 
-  inc rcx             ; increment counter by 1
-  cmp rcx, r11        ; if the counter is less than 4
-  jle loop2           ; jump to loop2
-  ret
+	inc rcx; increment counter by 1
+	cmp rcx, r11; if the counter is less than 4
+	jle loop2; jump to loop2
+	ret
 
-end: 
-  mov rax, 60         ; system call for exit
-  mov rdi, 0          ; exit code 0 
-  syscall
+end:
+	mov rax, 60; system call for exit
+	mov rdi, 0; exit code 0
+	syscall
